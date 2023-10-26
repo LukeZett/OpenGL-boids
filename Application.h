e949a5df2ce5a8dec1670d1fd7dcc3d9e4c377fa
Luke <lukas.zaborsky@gmail.com>
2023-10-26 15:35:48 +0200

    Add camera struct

--- Application.h
+++ Application.h
@@ -1,6 +1,29 @@
 #pragma once
 #include <GL/glew.h>
 #include "Simulation.h"
+#include <glm/ext.hpp>
+
+struct Camera {
+	glm::vec3 position;
+	glm::vec3 looking_dir;
+	float fov;
+	float aspect_ratio;
+	glm::vec3 up_dir = {0.0f, 1.0f, 0.0f};
+	float z_near = 0.01f;
+	float z_far = 100.0f;
+
+	glm::mat4x4 GetPerpective()
+	{
+		return glm::perspective(fov, aspect_ratio, z_near, z_far);
+	}
+
+	glm::mat4x4 GetView()
+	{
+		return glm::lookAt(position, position + looking_dir, up_dir);
+	}
+
+
+};
 
 class Application
 {
